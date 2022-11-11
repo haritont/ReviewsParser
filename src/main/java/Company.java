7f69e0b52cea310661ca962df84bda5ea112b2b0
Vika <haritont180402@gmail.com>
2022-11-11 18:57:52 +0300

    Написала поля и сеттеры и геттеры для них в классе Company

--- src/main/java/Company.java
+++ src/main/java/Company.java
@@ -1,2 +1,22 @@
+import java.util.ArrayList;
+
 public class Company {
+    private String name;
+    private ArrayList<String> reviews;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setReviews(ArrayList<String> reviews) {
+        this.reviews = reviews;
+    }
+
+    public ArrayList<String> getReviews() {
+        return reviews;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
