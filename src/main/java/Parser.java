1ebd7e5b3fdd50def5910e4a6adb1106181782fb
Vika <haritont180402@gmail.com>
2022-11-11 18:44:08 +0300

    Написала конструктор класса Parser. Добавила сеттер для поля url

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,2 +1,16 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Parser {
+    private String url;
+    private WebDriver driver;
+
+    public Parser(){
+        System.setProperty("webdriver.chrome.driver", "selenium\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
