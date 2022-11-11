911b082eafa71942f5923c51e9a609573ff637e8
Vika <haritont180402@gmail.com>
2022-11-11 19:06:24 +0300

    Спарсила названия компаний в методе Work

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -9,6 +9,8 @@ public class Parser {
     private String url;
     private WebDriver driver;
 
+    private ArrayList<Company> companiesReviews;
+
     public Parser(){
         System.setProperty("webdriver.chrome.driver", "selenium\\chromedriver.exe");
         driver = new ChromeDriver();
@@ -25,6 +27,13 @@ public class Parser {
 
     private void Work(){
         ArrayList<WebElement> companies = (ArrayList<WebElement>) driver.findElements(new By.ByXPath("//*[@id=\"main\"]/div[1]/div/div"));
+        for (int i =1; i<=companies.size();i++){
+            Company company = new Company();
+            WebElement companyWebElement = companies.get(i-1);
+            company.setName(companyWebElement.getText());
+            companyWebElement.click();
+            companiesReviews.add(company);
+        }
     }
 
     public void End(){
